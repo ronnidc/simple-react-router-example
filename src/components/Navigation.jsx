7c9af3bb95290399d0630a5dfed8cdd0b74b4ed6
ronnidc (RDC) <ronnidc@gmail.com>
2019-07-24 01:39:38 +0200

    add styling to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
 	return (
-		<div>
+		<div className="navigation">
 			<NavLink to="/" >Home</NavLink>
 			<NavLink to="/about" >About</NavLink>
 			<NavLink to="/contact" >Contact</NavLink>
